c080931ae6bfaa98ffe59a30e1acb2885765ca05
Jovica Veljkovic <166545932+jovica-me@users.noreply.github.com>
2024-03-07 21:12:56 +0100

    Compose 1.6 DragAndDrop with dragAndDropSource and dragAndDropTarget

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
 }
 
 android {
@@ -40,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
@@ -50,22 +51,21 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("io.coil-kt:coil-compose:2.5.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.coil.compose)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
 }
--- app/src/main/java/me/jovica/myapplication/MainActivity.kt
+++ app/src/main/java/me/jovica/myapplication/MainActivity.kt
@@ -1,20 +1,22 @@
 package me.jovica.myapplication
 
+import android.content.ClipData
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.draganddrop.dragAndDropSource
+import androidx.compose.foundation.draganddrop.dragAndDropTarget
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
@@ -26,23 +28,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draganddrop.DragAndDropEvent
+import androidx.compose.ui.draganddrop.DragAndDropTarget
+import androidx.compose.ui.draganddrop.DragAndDropTransferData
+import androidx.compose.ui.draganddrop.toAndroidDragEvent
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.round
-import androidx.compose.ui.unit.toIntRect
-import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.rememberAsyncImagePainter
 import me.jovica.myapplication.ui.theme.MyApplicationTheme
 
 
@@ -64,15 +66,16 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun App(photosViewModel: MainViewModel = viewModel()) {
 
     val photos by photosViewModel.photosList.collectAsState()
-    val selectedImage = remember { mutableStateOf(-1) }
+    val selectedImage = remember { mutableIntStateOf(-1) }
     val currentImageOffset = remember { mutableStateOf(Offset.Zero) }
     val lazyGridState = rememberLazyGridState()
 
-    val youWin by remember { derivedStateOf { photos.equals(me.jovica.myapplication.photos) } }
+    val youWin by remember { derivedStateOf { photos == me.jovica.myapplication.photos } }
     val haptic = LocalHapticFeedback.current
 
     Column {
@@ -80,61 +83,59 @@ fun App(photosViewModel: MainViewModel = viewModel()) {
             columns = GridCells.Fixed(3),
             state = lazyGridState,
             verticalArrangement = Arrangement.spacedBy(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            modifier = Modifier.gridDragAndDrop(
-                lazyGridState = lazyGridState,
-                setSelectedImage = {
-                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                    selectedImage.value = it
-                },
-                setCurrentOffset = {
-                    currentImageOffset.value = it
-                },
-                swapItems = { id1, id2 ->
-                    photosViewModel.swapItems(id1, id2)
-                }
-            )
-
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
         ) {
 
             items(photos, key = { it.id }) { photo ->
-                val selected by remember { derivedStateOf { selectedImage.value == photo.id } }
-                val animatedAlpha by animateFloatAsState(
-                    targetValue = if (selected) 1.0f else 0.8f,
-                    label = "alpha"
-                )
+                var backgroundColor by remember { mutableStateOf(Color.Transparent) }
+
                 Box(
                     modifier = Modifier
-                        .graphicsLayer {
-                            if (selected) {
-                                translationX = -currentImageOffset.value.x
-                                translationY = -currentImageOffset.value.y
-                                alpha = animatedAlpha * 0.8f
-                                scaleX = animatedAlpha
-                                scaleY = animatedAlpha
-                            }
-                        }
                         .aspectRatio(1f)
-                        .background(
-                            Brush.horizontalGradient(
-                                colorStops = arrayOf(
-                                    0.0f to Color.Yellow,
-                                    0.2f to Color.Red,
-                                    1f to Color.Blue
-                                )
-                            )
-                        )
-                        .then(
-                            if (selectedImage.value == photo.id) {
-                                Modifier.zIndex(1f)
-                            } else {
-                                Modifier
+                        .dragAndDropTarget(
+                            shouldStartDragAndDrop = { startEvent ->
+                                true
+                            },
+                            target = object : DragAndDropTarget {
+                                override fun onStarted(event: DragAndDropEvent) {
+                                    backgroundColor = Color.DarkGray.copy(alpha = 0.2f)
+                                }
+
+                                override fun onDrop(event: DragAndDropEvent): Boolean {
+                                    val dragedIndex =
+                                        event.toAndroidDragEvent().clipData.getItemAt(0).text
+                                            .toString()
+                                            .toInt()
+                                    photosViewModel.swapItems(dragedIndex, photo.id)
+                                    return true
+                                }
+
+                                override fun onEnded(event: DragAndDropEvent) {
+                                    backgroundColor = Color.Transparent
+                                }
                             }
                         )
+                        .dragAndDropSource {
+                            detectTapGestures(
+                                onLongPress = {
+                                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                                    startTransfer(
+                                        DragAndDropTransferData(
+                                            clipData = ClipData.newPlainText(
+                                                "text/",
+                                                "${photo.id}"
+                                            ),
+                                            flags = View.DRAG_FLAG_GLOBAL,
+                                        )
+                                    )
+                                }
+                            )
+                        }
+                        .background(backgroundColor)
 
                 ) {
                     Image(
-                        painter = rememberAsyncImagePainter(photo.url),
+                        painter = painterResource(photo.url),
                         contentDescription = photo.description,
                         contentScale = ContentScale.Crop,
                         modifier = Modifier.fillMaxSize()
@@ -143,78 +144,12 @@ fun App(photosViewModel: MainViewModel = viewModel()) {
                 }
             }
         }
-
         if (youWin) {
             Text(text = "You Win")
             Button(onClick = { photosViewModel.reset() }) {
                 Text(text = "Reset")
             }
         }
-
     }
+
 }
-
-
-fun Modifier.gridDragAndDrop(
-    lazyGridState: LazyGridState,
-    setCurrentOffset: (Offset) -> Unit = {},
-    swapItems: (id1: Int, id2: Int) -> Unit = { _, _ -> },
-    setSelectedImage: (Int) -> Unit
-) =
-    pointerInput(Unit) {
-
-        fun photoIdAndOffset(hitPoint: Offset): Int? {
-            return lazyGridState.layoutInfo.visibleItemsInfo.find { itemInfo ->
-                itemInfo.size.toIntRect().contains(hitPoint.round() - itemInfo.offset)
-            }?.key as? Int
-        }
-
-
-        var selectedPhotoId: Int? = null
-        var dropedPhotoId: Int? = null
-
-        var selectedPhotoOffset: Offset = Offset.Zero
-
-        detectDragGesturesAfterLongPress(
-
-            onDragStart = { offset ->
-
-                photoIdAndOffset(offset)?.let { photoId ->
-                    selectedPhotoId = photoId
-                    dropedPhotoId = photoId
-                    setSelectedImage(selectedPhotoId!!)
-
-                    selectedPhotoOffset = offset
-                    Log.d("Drag", "Drag started at $offset with photoId $photoId")
-                }
-
-            },
-
-            onDrag = { change, dragAmount ->
-                setCurrentOffset(selectedPhotoOffset - change.position)
-                if (selectedPhotoId != null) {
-                    photoIdAndOffset(change.position)?.let { pointerPhotoId ->
-                        if (dropedPhotoId != pointerPhotoId) {
-                            dropedPhotoId = pointerPhotoId
-                            Log.d("Drag", "Dragged to ${dragAmount} with photoId $selectedPhotoId")
-                        }
-                    }
-                }
-            },
-            onDragEnd = {
-                swapItems(selectedPhotoId!!, dropedPhotoId!!)
-                selectedPhotoOffset = Offset.Zero
-                setCurrentOffset(Offset.Zero)
-                setSelectedImage(-1)
-                selectedPhotoId = null
-                Log.d("Drag", "Drag ended with photoId $dropedPhotoId")
-            },
-            onDragCancel = {
-                selectedPhotoOffset = Offset.Zero
-                setSelectedImage(-1)
-                setCurrentOffset(Offset.Zero)
-                selectedPhotoId = null
-                Log.d("Drag", "Drag cancelled with photoId $dropedPhotoId")
-            },
-        )
-    }
